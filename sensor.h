4361d23b95b2ed83f6677763ec572419fb2ad207
HieuD21 <157302092+HieuD21@users.noreply.github.com>
2026-06-04 17:07:23 +0700

    Update sensor.h
    Add sensor header file

--- sensor.h
+++ sensor.h
@@ -1,1 +1,21 @@
+#ifndef SENSOR_H
+#define SENSOR_H
 
+typedef enum {
+    SENSOR_OK = 0,
+    SENSOR_WARNING = 1,
+    SENSOR_ERROR = 2
+} SensorStatus;
+
+typedef struct {
+    int temperature;
+    int voltage;
+    SensorStatus status;
+} SensorData;
+
+int is_temperature_valid(int temperature);
+int is_voltage_valid(int voltage);
+SensorStatus classify_sensor_status(int temperature, int voltage);
+const char* status_to_string(SensorStatus status);
+
+#endif
